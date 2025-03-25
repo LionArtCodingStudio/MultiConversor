f4ec64bd96596e1097bc668b2e3d618a0712c152
LionArtCodingStudio <leonardoweb25@gmail.com>
2025-03-25 17:51:28 -0300

    Add files via upload

---
+++ src/javabinaryconversor/Conversores.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javabinaryconversor;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author leoga
+ */
+public class Conversores {
+    
+        public static String dec_bi(String numero){
+            String resB;
+            String numStr = numero;
+    
+            double N = Double.parseDouble(numStr);
+            //listas------------------------------------------------------------
+            ArrayList<Double> divisores = new ArrayList<>();
+            ArrayList<Integer> binario = new ArrayList<>();
+            
+            for(double i = 1; i <= N;i++){
+                if (N%i==0){
+                    divisores.add(i);
+                } 
+            }
+            //converte para binario---------------------------------------------
+            double tempnum = N;
+            while (tempnum > 0){
+                int sobra = (int)(tempnum % 2);
+                binario.add(sobra);
+                tempnum /=2;
+            }
+            //inverte os numeros para formar um binario-------------------------
+            StringBuilder revBinarioStr = new StringBuilder();
+            for (int i = binario.size() -1; i >= 0; i--){
+                revBinarioStr.append(binario.get(i));
+            }  
+            //Lembrete
+            //Exemplo numero 1812
+            // - 1 e usado pois .size conta o tamanho do array a partir de 1
+            // ele subtrai o valor a partir do index 11 até o 1 
+            // mas o array começa a partir do index 0 e vai ate 10
+            // no caso do numero 1812 a var binario retorna 00101000111 (x=nada)
+            //  bits nao invertidos        = 0  0  1  0  1  0  0  0  1  1  1  x
+            //                               ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
+            //index(local) no array        = 0  1  2  3  4  5  6  7  8  9  10 x
+            //binario.size retorna         = x  1  2  3  4  5  6  7  8  9  10 11
+            //                                  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
+            //     sem subtrair ele para aqui-> 1  1  1  0  0  0  1  0  1  0  
+            //         e como não existe index 11 no array ele começa aqui ^
+            //se voce subitrai cada valor do binari.size por -1 ele fica assim
+            //index(local) no array        = 0  1  2  3  4  5  6  7  8  9  10 
+            //binario.size-1 retorna       = 0  1  2  3  4  5  6  7  8  9  10 
+            //                               ^  ^  ^  ^  ^  ^  ^  ^  ^  ^  ^
+            //bits nao invertidos          = 0  0  1  0  1  0  0  0  1  1  1
+            //------------------------------------------------------------------
+            //converte o array para string--------------------------------------
+            String divisoresStr = String.join("-> ", 
+                divisores.stream()
+                        .map(Object::toString)
+                            .toArray(String[]::new));
+            
+            String binarioStr = revBinarioStr.toString();
+            if (binarioStr.contains("1")) {
+                binarioStr = binarioStr.replaceFirst("^0+(?!$)", "");
+            }
+            resB=binarioStr;
+            return resB;
+            //chama o BinarioStr e divisoresStr em suas respectivas textAreas
+            //ou o output de sua escolha casa esteja usando fora do netbeans
+    }
+        public static String bi_dec(String numero){
+            String resDec="";
+            
+            return resDec;
+        }
+}
---
+++ src/javabinaryconversor/JavaBinaryConversor.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package javabinaryconversor;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/**
+ *
+ * @author leoga
+ */
+public class JavaBinaryConversor extends Conversores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        //Criar o frame------------------------------
+        JFrame frame = new JFrame();
+        
+        frame.setSize( 420, 420);
+        frame.setTitle("Java Conversor - VER 0.1");
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.setLayout(null);
+	frame.setResizable(false);
+	frame.setVisible(true);
+        //__________________________________________
+        //conversão de decimal para binario
+        JLabel conv_dec_bi = new JLabel("Decimal => Binario");
+        conv_dec_bi.setBounds(15,2,200,  15   );
+        frame.add(conv_dec_bi);
+        conv_dec_bi.setVisible(true);
+        
+        //input do usuario (decimal)----------------
+        //label
+        JLabel lbNum = new JLabel("Coloque um numero: ");
+        lbNum.setVisible(true);
+        lbNum.setBounds(15,15,200,  15   );
+        frame.add(lbNum);
+        //Campo de texto
+        JTextField txtNumero = new JTextField();
+        txtNumero.setBounds(15,30,165,  15);
+        
+        String txtNum=txtNumero.getText();
+        
+        
+        frame.add(txtNumero);   
+        
+        //------------------------------------------
+        //output resultado (Binario)
+        //label
+        JLabel lbBinario = new JLabel("Binario => ");
+        lbBinario.setBounds(15,60,100,  15   );
+        frame.add(lbBinario);
+        lbBinario.setVisible(true);
+        //Campo de texto
+        JTextField resultado = new JTextField();
+        resultado.setEditable(false);
+        resultado.setBounds(80,60,100,  15);
+        frame.add(resultado);
+        //------------------------------------------
+        //Botão comfirmar converção
+        JButton converter = new JButton("converter");
+        converter.setBounds(15,45,165,15);
+        converter.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+               resultado.setText("");
+               String result = dec_bi(txtNumero.getText());
+               resultado.setText(result);
+               System.out.println("Resultado = "+result);
+            }
+        });
+        frame.add(converter);
+        
+        
+        
+        
+    }
+    
+}
